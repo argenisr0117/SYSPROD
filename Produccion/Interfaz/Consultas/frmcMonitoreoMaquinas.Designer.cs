9f6ff7ef6aa5353ac77984a46b660dd0f24b5c5e
Argenis <argenis0117@hotmail.com>
2018-06-26 15:49:22 -0400

    actual

--- Produccion/Interfaz/Consultas/frmcMonitoreoMaquinas.Designer.cs
+++ Produccion/Interfaz/Consultas/frmcMonitoreoMaquinas.Designer.cs
@@ -33,6 +33,9 @@
             this.lbTitulo = new System.Windows.Forms.Label();
             this.EstadoMaqTimer = new System.Windows.Forms.Timer(this.components);
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbTitulo
@@ -43,21 +46,39 @@
             this.lbTitulo.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lbTitulo.Location = new System.Drawing.Point(278, 9);
             this.lbTitulo.Name = "lbTitulo";
-            this.lbTitulo.Size = new System.Drawing.Size(371, 26);
+            this.lbTitulo.Size = new System.Drawing.Size(283, 26);
             this.lbTitulo.TabIndex = 68;
-            this.lbTitulo.Text = "MONITOREO DE MÁQUINAS [TURNO:";
+            this.lbTitulo.Text = "MONITOREO DE MÁQUINAS";
             // 
             // EstadoMaqTimer
             // 
             this.EstadoMaqTimer.Interval = 1000;
             this.EstadoMaqTimer.Tick += new System.EventHandler(this.EstadoMaqTimer_Tick);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 44);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(839, 464);
+            this.flowLayoutPanel1.TabIndex = 69;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbTitulo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(839, 44);
+            this.panel1.TabIndex = 70;
+            // 
             // frmcMonitoreoMaquinas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(839, 508);
-            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -66,8 +87,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmcMonitoreoMaquinas_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -76,5 +98,7 @@
         private System.Windows.Forms.Label lbTitulo;
         private System.Windows.Forms.Timer EstadoMaqTimer;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
